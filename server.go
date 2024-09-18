8aa46a4182f14115f940ad9d3c2a00207e1fd997
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-09-18 11:37:08 +0200

    revert changes

--- server.go
+++ server.go
@@ -37,35 +37,16 @@ func (s *Server) Listen() error {
 			return err
 		}
 
-		//errChan := make(chan error)
 		go s.processRequest(conn)
-
-		/* for err = range errChan {
-			if err != nil {
-				fmt.Println(err) // panic?
-			}
-		} */
 	}
 }
 
 func (s *Server) processRequest(conn net.Conn) {
-	// defer close(errChan)
-	/* defer func() {
-		if err := conn.Close(); err != nil {
-			fmt.Println(err)
-			// errChan <- err
-			return
-		}
-	}() */
 	defer conn.Close()
 
 	request, err := parseRequest(conn)
 	if err != nil {
-		_, err := conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
-		if err != nil {
-			fmt.Println(err)
-			// errChan <- err
-		}
+		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
 		return
 	}
 
@@ -74,24 +55,15 @@ func (s *Server) processRequest(conn net.Conn) {
 		response := NewHttpResponse()
 		handler(response, request)
 		responseString := response.assembleResponseString()
-		_, err = conn.Write([]byte(responseString))
-		if err != nil {
-			fmt.Println(err)
-			// errChan <- err
-		}
+		conn.Write([]byte(responseString))
 		return
 	}
 
-	_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-	if err != nil {
-		fmt.Println(err)
-		// errChan <- err
-	}
+	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 }
 
 func parseRequest(conn net.Conn) (*HttpRequest, error) {
 	lines := make(chan []byte)
-	defer close(lines)
 	go byteReader(lines, conn)
 	requestLineValues := bytes.Split(<-lines, []byte(" "))
 
@@ -134,6 +106,7 @@ func parseRequest(conn net.Conn) (*HttpRequest, error) {
 }
 
 func byteReader(channel chan []byte, connection net.Conn) {
+	defer close(channel)
 	buffer := make([]byte, 0)
 
 	for {
